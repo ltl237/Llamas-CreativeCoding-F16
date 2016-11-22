6ecb57696a5b8e97ad17690a0e2b562bad80ed49
Lorenzo <ltl237@nyu.edu>
2016-11-22 05:02:35 -0500

    done

--- 11.17 hw/sketch.js
+++ 11.17 hw/sketch.js
@@ -6,47 +6,34 @@ var strArray;
 var url;
 var root;
 var api;
-var weatherMain;
+var globalData;
 function preload(){
-	root = "http://api.openweathermap.org/data/2.5/weather?q=";
-	api = "&appid=0ddd5705acf924678dd5810004c1800a";
-	query = "London"
-	strArray = ["NewYork","Rockland","SanDiego","Charlotte"];
-	index = 0;
-	url = root + strArray[index] + api;
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=New%20York,ny&appid=89309063680894bde99f0b41e0e6ccbc";
 	myData = loadJSON(url);
 	
 }
 function setup(){
 	createCanvas(600,600);
 	background(200);
-	
 
+	root = "http://api.openweathermap.org/data/2.5/weather?q=";
+	api = "&appid=0ddd5705acf924678dd5810004c1800a";
+	strArray = ["NewYork","Rockland","SanDiego","Charlotte"];
+	url = root + strArray[index] + api;
+	myData = loadJSON(url,gotData);
 }
 function draw(){
 	
 }
- 
-function Rectangle(dataHeight){
-	this.drawRect = function(){
-		// this.dataHeight = temperature;
-		this.x = mouseX;
-		this.y = mouseY;
-		rect(this.x,this.y,100,dataHeight/2);
-	}
+
+function gotData(myData){
+	globalData = myData;
 }
 
 function mouseClicked(){
 	index++;
-	strArray = ["NewYork","Rockland","SanDiego","Charlotte"];
 	url = root + strArray[index] + api;
-
-	temperature = myData.main.temp;
-	weatherMain = myData.main.humidity;
-	if(weatherMain < 51){
-		ellipse(mouseX,mouseY-20,30,30);
-	}
-	console.log(weatherMain);
+	myData = loadJSON(url,gotData);
 	
 	rectObj = new Rectangle(temperature);
 	rectObj.drawRect();
@@ -54,5 +41,15 @@ function mouseClicked(){
 		index = 0;
 	}
 	console.log(url);
-	console.log(temperature);
+	console.log(index);
+	console.log(strArray[index]);
+}
+
+function Rectangle(dataHeight){
+	this.drawRect = function(){
+		// this.dataHeight = temperature;
+		this.x = mouseX;
+		this.y = mouseY;
+		rect(this.x,this.y,100,dataHeight/2);
+	}
 }
---
+++ 11.17 hw/sketch2.js
@@ -0,0 +1,110 @@
+
+var myData;
+var globalData;
+
+var url;
+var baseUrl;
+var city;
+var APIkey;
+
+var strCities = ["NewYork","Rockland","SanDiego","Seattle"];
+var index = 0;
+
+var imageObj;
+var temperature;
+var cloudNumber;
+function preload(){
+	//load JSON formatted data from URL
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=New%20York,ny&appid=89309063680894bde99f0b41e0e6ccbc";
+	// var url = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/web_colors.json'
+	myData = loadJSON(url);
+	cloud = loadImage("cloud.svg");
+	empire = loadImage("empire.jpg");
+	rock = loadImage("rock.jpg");
+	beach = loadImage("beach.png");
+	rain = loadImage("rain.png");
+}
+
+
+function setup(){
+
+	createCanvas(600,600);
+	// background(200.5);
+	baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
+	//baseUrl = "http://netflixroulette.net/api/api.php?";
+	city = "New York,ny";
+	APIkey = "89309063680894bde99f0b41e0e6ccbc";
+
+	url = baseUrl + "q=" + city + "&appid=" + APIkey;
+
+	myData = loadJSON(url, gotData);
+	imageObj = new Images();
+}
+
+function gotData(myData){
+	// do something with this data
+	globalData = myData;
+
+	// console.log(globalData);
+	temperature = globalData.main.temp;
+	cloudNumber = globalData.clouds.all;
+}
+
+function draw(){
+
+
+}
+
+function mouseClicked(){
+	index++;
+	imageObj.drawCloud();
+	url = baseUrl + "q=" + strCities[index] + "&appid=" + APIkey;
+
+	myData = loadJSON(url, gotData);
+	if(index > 3){
+		index = 0;
+	}
+	// console.log(temperature/2);
+}
+
+function keyReleased(){
+	imageObj.drawElements();
+}
+
+function Images(){
+	this.drawCloud = function(cloudNum){
+		this.x = mouseX;
+		this.y = mouseY;
+		this.cloudNum = cloudNumber;
+		if(cloudNumber < 20){
+			// cloudNumber = 10;
+			image(cloud,this.x,this.y,30,30);
+		}
+		else{
+			fill(temperature/2);
+			// console.log(temperature);
+			// rect(this.x,this.y,cloudNumber,cloudNumber);
+			image(cloud,this.x,this.y,cloudNumber,cloudNumber);
+
+		}
+	}
+	this.drawElements = function(temp){
+		this.x = mouseX;
+		this.y = mouseY;
+		this.temp = temperature/4;
+
+		if(index == 0){
+			image(empire,this.x,this.y,this.temp,this.temp);
+		}
+		else if(index == 1){
+			image(rock,this.x,this.y,this.temp,this.temp);
+		}
+		else if(index == 2){
+			image(beach,this.x,this.y,this.temp,this.temp);
+		}
+		else if(index == 3){
+			image(rain,this.x,this.y,this.temp,this.temp);
+		}
+	}
+}
+
